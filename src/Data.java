4a60eb245119241b5ddf34a8c70452939015c835
Sallai Andras <andrewteki@gmail.com>
2022-09-05 13:45:54 +0200

    Mariadb elérés kész

---
+++ src/Data.java
@@ -0,0 +1,10 @@
+public class Data {
+    Database database;
+    public Data(Database database) {
+        this.database = database;
+    }
+    public void connect() {
+        this.database.connectDb();
+    }
+    
+}
